ae6fd9d64561a6e339cdb7e0c71f6fdc8b8d5fd1
rahulsps <rahul_bhola@softprodigy.com>
2020-04-01 15:59:04 +0530

    create connect url

--- src/server/routes/ui/index.js
+++ src/server/routes/ui/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const async = require('async');
+const config = require('config');
 const HTTPStatus = require('http-status');
 
 module.exports = function (app, wagner) {
@@ -16,7 +17,8 @@ module.exports = function (app, wagner) {
     app.get('/', function (req, res) {
 
       res.render('index', {
-          title: 'Discord Messaging - Home'
+          title: 'Discord Messaging - Home',
+          connect_url: `${config.discord.api_host}${config.discord.auth_ep}?client_id=${config.discord.client_id}&redirect_uri=${config.app_route}&response_type=code&scope=${config.discord.oauth2_auth_scope}`,
       });
 
     });
