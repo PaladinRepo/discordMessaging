41a7808ddc933fce3864c1f08fdb5213f2772fb9
rahulsps <rahul_bhola@softprodigy.com>
2020-04-01 17:35:50 +0530

    update user migration

--- migrations/20200331093626-users.js
+++ migrations/20200331093626-users.js
@@ -1,5 +1,13 @@
 'use strict';
-
+/*
+{
+  "access_token": "huSqlZj7YLMZpbuf0fCo5hiWgF4gbM",
+  "expires_in": 604800,
+  "refresh_token": "YpMne4xBkBr2r3kCy40m48kFerc5Fn",
+  "scope": "identify email messages.read",
+  "token_type": "Bearer"
+}
+*/
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return  queryInterface.createTable(
@@ -24,16 +32,36 @@ module.exports = {
           unique: true,
           allowNull: true
         },
-        token: {
-          type: Sequelize.STRING(255),
-          unique: true,
-          allowNull: true
-        },
         email: {
           type: Sequelize.STRING(255),
           unique: true,
           allowNull: true
-        }
+        },
+        access_token: {
+          type: Sequelize.STRING(255),
+          unique: true,
+          allowNull: true
+        },
+        refresh_token: {
+          type: Sequelize.STRING(255),
+          unique: true,
+          allowNull: true
+        },
+        expires_in: {
+          type: Sequelize.STRING(255),
+          unique: true,
+          allowNull: true
+        },
+        scope: {
+          type: Sequelize.STRING(255),
+          unique: true,
+          allowNull: true
+        },
+        token_type: {
+          type: Sequelize.STRING(255),
+          unique: true,
+          allowNull: true
+        },
       },
       {
         engine: 'InnoDB',    // default: 'InnoDB'
--- src/server/models/User.js
+++ src/server/models/User.js
@@ -17,16 +17,36 @@ var User = sequelize.define("User",
       unique: true,
       allowNull: true
     },
-    token: {
-      type: Sequelize.STRING(255),
-      unique: true,
-      allowNull: true
-    },
     email: {
       type: Sequelize.STRING(255),
       unique: true,
       allowNull: true
-    }
+    },
+    access_token: {
+      type: Sequelize.STRING(255),
+      unique: true,
+      allowNull: true
+    },
+    refresh_token: {
+      type: Sequelize.STRING(255),
+      unique: true,
+      allowNull: true
+    },
+    expires_in: {
+      type: Sequelize.STRING(255),
+      unique: true,
+      allowNull: true
+    },
+    scope: {
+      type: Sequelize.STRING(255),
+      unique: true,
+      allowNull: true
+    },
+    token_type: {
+      type: Sequelize.STRING(255),
+      unique: true,
+      allowNull: true
+    },
   }, {
     tableName: 'discord_users',
     timestamps: true,
